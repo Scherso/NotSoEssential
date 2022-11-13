e84e0244073c447823b435d870ed727584e57035
Scherso <Scherso@users.noreply.github.com>
2022-11-13 16:13:40 -0500

    Remove Essential Cosmetic Rendering

--- src/main/java/com/github/u9g/notsoessential/asm/transformer/EssentialModelRendererTransformer.java
+++ src/main/java/com/github/u9g/notsoessential/asm/transformer/EssentialModelRendererTransformer.java
@@ -1,9 +1,8 @@
 package com.github.u9g.notsoessential.asm.transformer;
 
 import com.github.u9g.notsoessential.asm.ITransformer;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
 
 import java.util.ListIterator;
 
@@ -11,20 +10,33 @@ public class EssentialModelRendererTransformer implements ITransformer {
 
     @Override
     public String[] getClassName() {
-        return new String[]{"gg.essential.cosmetics.EssentialModelRenderer"};
+        return (new String[]{"gg.essential.cosmetics.EssentialModelRenderer"});
     }
 
     @Override
     public void transform(ClassNode classNode, String name) {
         for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals("render")) {
+                methodNode.localVariables.clear();
+                methodNode.instructions.clear();
+                methodNode.instructions.insertBefore(methodNode.instructions.getFirst(), new InsnNode(Opcodes.RETURN));
+            }
             if (methodNode.name.equals("cosmeticsShouldRender")) {
                 final ListIterator<AbstractInsnNode> iterator = methodNode.instructions.iterator();
                 while (iterator.hasNext()) {
-                    final AbstractInsnNode node = iterator.next();
-
+                    final AbstractInsnNode next = iterator.next();
+                    if (next instanceof VarInsnNode && next.getOpcode() == Opcodes.DSTORE)
+                        methodNode.instructions.insertBefore(next.getNext(), InstructionList());
                 }
             }
         }
+    }
+
+    private InsnList InstructionList() {
+        InsnList list = new InsnList();
+        list.add(new InsnNode(Opcodes.ICONST_0));
+        list.add(new InsnNode(Opcodes.IRETURN));
+        return list;
     }
 
 }
