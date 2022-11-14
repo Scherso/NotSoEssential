263ac2e6110b0b35bc22baf935154567edd1d290
Sam <90007553+Scherso@users.noreply.github.com>
2022-11-14 15:15:44 -0500

    Update ITransformer.java

--- src/main/java/com/github/u9g/notsoessential/asm/ITransformer.java
+++ src/main/java/com/github/u9g/notsoessential/asm/ITransformer.java
@@ -85,12 +85,10 @@ public interface ITransformer {
         methodNode.instructions.clear();
 
         /* Prevent procedure from clearing an already empty local variable. */
-        if (!methodNode.localVariables.isEmpty())
-            methodNode.localVariables.clear();
+        if (!methodNode.localVariables.isEmpty()) methodNode.localVariables.clear();
 
         /* Prevent procedure from clearing an already empty catch block. */
-        if (!methodNode.tryCatchBlocks.isEmpty())
-            methodNode.tryCatchBlocks.clear();
+        if (!methodNode.tryCatchBlocks.isEmpty()) methodNode.tryCatchBlocks.clear();
     }
 
 }
