e7a556800d14426a135a4c023d2bbca34fe83e33
Scherso <Scherso@users.noreply.github.com>
2022-12-08 23:00:20 -0500

    Remove unnecessary repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,10 @@ pluginManagement {
     repositories {
         mavenCentral()
         gradlePluginPortal()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
         maven("https://maven.architectury.dev/")
         maven("https://maven.fabricmc.net")
         maven("https://maven.minecraftforge.net/")
-        maven("https://repo.spongepowered.org/maven/")
-        maven("https://repo.sk1er.club/repository/maven-releases/")
+        maven("https://repo.essential.gg/repository/maven-public")
     }
     resolutionStrategy {
         eachPlugin {
