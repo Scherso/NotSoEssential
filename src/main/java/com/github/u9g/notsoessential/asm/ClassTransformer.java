b99d2f602953044a6826478ce0cb546ed3a77f05
Scherso <Scherso@users.noreply.github.com>
2022-12-26 22:15:08 -0500

    It's kinda scary that Sk1er starred this repo

--- src/main/java/com/github/u9g/notsoessential/asm/ClassTransformer.java
+++ src/main/java/com/github/u9g/notsoessential/asm/ClassTransformer.java
@@ -61,11 +61,11 @@ public class ClassTransformer implements IClassTransformer
 		final List<ITransformer> TRANSFORMER_LIST = this.TRANSFORMER_HASHMAP.get(transformedName);
 		if (TRANSFORMER_LIST == null) return (bytes);
 
-		for (int i = TRANSFORMER_LIST.size() - 1; i > -1; i--)
-		{
-			final ITransformer TRANSFORMER = TRANSFORMER_LIST.get(i);
-			final ClassNode    NODE        = new ClassNode();
-			final ClassReader  READER      = new ClassReader(bytes);
+		for (int i = TRANSFORMER_LIST.size() - 1; i > -1; i--)          /* For statement here matches a for-each, ex:          */
+		{                                                               /* for (ITransformer transformer : TRANSFORMER_LIST) { */
+			final ITransformer TRANSFORMER = TRANSFORMER_LIST.get(i);   /*     transformer.transform(node, transformedName);   */
+			final ClassNode    NODE        = new ClassNode();           /* }                                                   */
+			final ClassReader  READER      = new ClassReader(bytes);    /* Statement is written differently for performance.   */
 			READER.accept(NODE, 0);
 			TRANSFORMER.transform(NODE, transformedName);
 			final ClassWriter WRITER = new ClassWriter(READER, 0);
