fd231e7a1f404f3c75ad5b434d5cf58a8023339d
Scherso <90007553+Scherso@users.noreply.github.com>
2025-06-23 14:29:04 -0400

    Add comment to EssentialSetupPreLaunchMixin

--- NotSoEssential-Fabric/src/main/java/com/github/u9g/notsoessential/mixin/EssentialSetupPreLaunchMixin.java
+++ NotSoEssential-Fabric/src/main/java/com/github/u9g/notsoessential/mixin/EssentialSetupPreLaunchMixin.java
@@ -13,6 +13,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class EssentialSetupPreLaunchMixin
 {
 
+    /**
+     * Essential is not a typical mod; it loads dynamically from
+     * {@code .minecraft/essential} via a prelaunch entry point,
+     * even when bundled in the mod JAR. During prelaunch, it
+     * remains on the classpath since
+     * {@code gg.essential.loader.stage2.EssentialLoader#chainLoadMixins()}
+     * hasn't been called yet, allowing a configuration to be
+     * loaded alongside Essential.
+     *
+     * @param ci {@link CallbackInfo}
+     */
     @Inject(method = "onPreLaunch", at = @At("RETURN"), remap = false)
     private void nse$onPreLaunch(final CallbackInfo ci)
     {
